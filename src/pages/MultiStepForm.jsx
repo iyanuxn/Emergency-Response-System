cf42ab65bfc0bed05bd3f85fa7f908e57ee71cd2
Iyanu <iyanuxn@gmail.com>
2024-03-21 03:54:26 +0100

    map update

--- src/pages/MultiStepForm.jsx
+++ src/pages/MultiStepForm.jsx
@@ -2,12 +2,18 @@ import React, { useState, useEffect } from "react";
 import { FiChevronDown } from "react-icons/fi";
 import { Link } from "react-router-dom";
 import { MdEmergency, MdOutlineMyLocation } from "react-icons/md";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 function MultiStepForm() {
   const [emergency, setEmergency] = useState("What is your emergency?");
   const [showOtherEmergencies, setshowOtherEmergencies] = useState(false);
   const [fetchingLocation, setFetchingLocation] = useState(false);
   const [userLocation, setUserLocation] = useState("");
+  // lat and long usestate
+  const [lat, setLat] = useState("");
+  const [long, setLong] = useState("");
+
+  const API_KEY = "AIzaSyDThNsjdA602Dg9t8BdjKONZ - tb6nLqKfw";
 
   const [step, setStep] = useState(1);
   const [formData, setFormData] = useState({
@@ -27,12 +33,6 @@ function MultiStepForm() {
     }));
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Handle form submission logic here
-    console.log(formData);
-    // Example: You might send the form data to the server here
-  };
   const [disableNext, setDisableNext] = useState(true);
 
   useEffect(() => {
@@ -113,10 +113,12 @@ function MultiStepForm() {
           setFetchingLocation(true);
           console.log("Latitude is :", latitude);
           console.log("Longitude is :", longitude);
+          setLat(latitude);
+          setLong(longitude);
 
           try {
             const response = await fetch(
-              `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=AIzaSyDThNsjdA602Dg9t8BdjKONZ-tb6nLqKfw`
+              `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${API_KEY}`
             );
             const data = await response.json();
 
@@ -145,6 +147,21 @@ function MultiStepForm() {
       alert("Geolocation is not supported by this browser.");
     }
   };
+
+  const [timeLeft, setTimeLeft] = useState(10 * 60);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (timeLeft > 0) {
+        setTimeLeft(timeLeft - 1);
+      }
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, [timeLeft]);
+
+  const minutes = Math.floor(timeLeft / 60);
+  const seconds = timeLeft % 60;
 
   const renderStep = () => {
     switch (step) {
@@ -203,10 +220,10 @@ function MultiStepForm() {
                 <button
                   onClick={handleGetLocation}
                   disabled={fetchingLocation}
-                  className={`text-white h-12 px-5 flex items-center font-medium gap-2 rounded-xl hover:translate-x-2 hover:-translate-y-2 hover:shadow-[-10px_10px_0px_1px_rgba(0,0,0,1)] transition-all duration-200 ease-in-out ${
+                  className={`text-white h-12 px-5 flex items-center font-medium gap-2 rounded-xl  ${
                     fetchingLocation
                       ? "bg-red-300 cursor-not-allowed"
-                      : "bg-red-500"
+                      : "bg-red-500 hover:translate-x-2 hover:-translate-y-2 hover:shadow-[-10px_10px_0px_1px_rgba(0,0,0,1)] transition-all duration-200 ease-in-out"
                   }`}
                 >
                   {fetchingLocation
@@ -290,11 +307,71 @@ function MultiStepForm() {
                   ? "opacity-50 cursor-not-allowed"
                   : "hover:translate-x-2 hover:-translate-y-2 hover:shadow-[-10px_10px_0px_1px_rgba(0,0,0,0.3)] transition-all duration-200 ease-in-out"
               }`}
-                onClick={handleSubmit}
+                onClick={nextStep}
                 disabled={disableNext}
               >
                 Submit
               </button>
+            </div>
+          </div>
+        );
+      case 3:
+        return (
+          <div className="flex flex-col h-full gap-10">
+            <span>
+              <div className="flex justify-between items-center">
+                <h2 className="text-xl font-medium text-neutral-700 mb-2">
+                  Your Location
+                </h2>
+                <div className="flex items-center gap-2">
+                  <span className="text-blue-400 font-medium">
+                    Estimated Time of Arrival:
+                  </span>
+                  <div className="text-center font-bold">
+                    <span>
+                      {minutes < 10 ? "0" : ""}
+                      {minutes}
+                    </span>
+                    :
+                    <span>
+                      {seconds < 10 ? "0" : ""}
+                      {seconds}
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div className="text-neutral-700 text-sm w-[60%]">
+                Check the map below for closest emergency response location
+                while we dispatch help to your location
+              </div>
+            </span>
+            <div className="flex flex-col gap-4">
+              {/* render google map */}
+              <div className="h-[400px] -mt-4 rounded-2xl overflow-hidden">
+                <LoadScript googleMapsApiKey={API_KEY}>
+                  <GoogleMap
+                    mapContainerStyle={{
+                      height: "100%",
+                      width: "100%",
+                    }}
+                    center={{
+                      lat: lat,
+                      lng: long,
+                    }}
+                    zoom={17}
+                  >
+                    //my marker
+                    <Marker
+                      position={{
+                        lat: lat,
+                        lng: long,
+                      }}
+                      title="My Location"
+                    />
+                  </GoogleMap>
+                </LoadScript>
+              </div>
+              {/* timer */}
             </div>
           </div>
         );
@@ -305,7 +382,7 @@ function MultiStepForm() {
 
   return (
     <div className="bg-neutral-200 h-screen w-screen overflow-hidden flex items-center justify-center">
-      <div className="w-[50%] rounded-2xl p-6 border border-neutral-200 shadow-xl bg-white flex flex-col gap-6">
+      <div className="w-[60%] rounded-2xl p-6 border border-neutral-200 shadow-xl bg-white flex flex-col gap-6">
         <div className="flex items-center justify-between">
           <div className="flex items-center gap-3  font-semibold text-xl">
             <MdEmergency className="w-7 h-7" />
